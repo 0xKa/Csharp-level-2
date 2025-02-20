75a951ef339a896a615c4be58bd6e164c034e188
Reda Hilal <reda0x04@gmail.com>
2025-02-20 21:04:27 +0400

    day 2 - lamda expression, using statment

---
+++ 7-Lambda-Expression/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Lambda Expression: it is a concise way to write anonymous methods (functions without a name).
+//It simplifies delegate, LINQ, and event handling code.
+
+//(parameters) => expression;
+//=> is called the lambda operator (reads as "goes to").
+//The left side is the input parameters.
+//The right side is the expression or function body.
+
+namespace _7_Lambda_Expression
+{
+    internal class Program
+    {
+        public delegate double Operation(double num1, double num2);
+
+
+        public static void ExecuteOperation(string OperationName, double Num1, double Num2, Operation operation)
+            => Console.WriteLine(OperationName + " Operation Result: " + operation.Invoke(Num1, Num2));
+
+        public static Action<string, int> UserCard = (string name, int age) =>
+        {
+            Console.WriteLine("\n------------------");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine("------------------\n");
+        };
+    
+
+
+        static void Main(string[] args)
+        {
+            ExecuteOperation("Add", 04, 5, (double num1, double num2) => num1 + num2); 
+            ExecuteOperation("Sub", 20, 5, (double num1, double num2) => num1 - num2); 
+            ExecuteOperation("Mult", 07, 5, (double num1, double num2) => num1 * num2); 
+            ExecuteOperation("Div", 50, 5, (double num1, double num2) => num1 / num2);
+
+            UserCard.Invoke("Reda", 20);
+
+
+            Console.Read();
+        }
+    
+    }
+}
---
+++ 8-Using-Statement/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using screen = System.Console;
+using static System.Console;
+using System.IO;
+
+
+// [using] keyword is used for:
+// 1- Include/Import Library => using System; 
+// 2- Create an Alias => using screen = System.Console;
+// 3- Static Directive => using static System.Console;
+// 4- Resource Management -> used for closing database, file stream, etc... connections
+
+namespace _8_Using_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-1
+            Console.WriteLine("Hello");
+
+            //-2
+            screen.WriteLine("Hello");
+
+
+            //-3
+            WriteLine("Hello");
+
+            //-4
+            using (StreamWriter writer = new StreamWriter("example.txt"))
+            {
+                writer.WriteLine("Hello, C#!");
+                writer.Flush(); // Ensure data is written before reading
+            }
+            using (StreamReader reader = new StreamReader("example.txt"))
+            {
+                string content = reader.ReadToEnd();
+                Console.WriteLine($"File Content: {content}");
+            } 
+            // Both writer and reader are disposed ✅
+
+            System.Console.ReadKey();
+        }
+    }
+}
