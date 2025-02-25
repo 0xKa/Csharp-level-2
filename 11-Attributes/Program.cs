bd28767721268d95be503cf4bf6597aed0630b40
Reda Hilal <reda0x04@gmail.com>
2025-02-25 16:44:29 +0400

    day 4

---
+++ 11-Attributes/Program.cs
@@ -0,0 +1,137 @@
+﻿#define Enable_Func1
+#define Enable_Func2
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ In C#, attributes are special metadata annotations that add additional information to 
+classes, methods, properties, parameters, and other code elements. 
+They allow you to attach declarative information that can be used at runtime using reflection.
+
+Think of attributes as labels that provide extra instructions 
+for how code should behave, be processed, or be documented.
+ */
+
+//Common Attributes:
+/*
+ * [Serializable] --> Applied to a class to indicate that its instances can be serialized.
+ * [NonSerialized] --> Applied to a field to indicate that it should not be serialized.
+ * [Conditional("")] --> To conditionally include or exclude methods from compilation based on the specified compilation symbols (string).
+ * [Obsolete("")] --> Used to mark program entities (such as classes, methods, properties, etc.) that are considered obsolete or deprecated.
+ * [AttributeUsage()] --> To create a custom attribute 
+ */
+
+[Serializable]
+public class Serializable_Example
+{
+    // Will be serialized
+    public int SerializedField = 32;
+
+
+    // Will not be serialized
+    [NonSerialized]
+    public int NonSerializedField = -32;
+}
+
+public class Conditional_Example
+{
+    [Conditional("DEBUG")] //will execute the method only in Debug mode.
+    public static void PrintName(string name)
+    {
+        Console.WriteLine(name);
+    }
+    public static void PrintAge(int age)
+    {
+        Console.WriteLine(age);
+    }
+
+
+    [Conditional("Enable_Func1")] //will execute the func is the string is defined in the program (check line 1)
+    public static void Func1()
+    {
+        Console.WriteLine("Func1 is Enabled.");
+
+    }
+    
+    [Conditional("Enable_Func2")] 
+    public static void Func2()
+    {
+        Console.WriteLine("Func2 is Enabled.");
+    }
+
+    public static void Show()
+    {
+        Conditional_Example.PrintName("Reda");
+        Conditional_Example.PrintAge(20);
+
+        Conditional_Example.Func1();
+        Conditional_Example.Func2();
+    }
+
+}
+
+[Obsolete("this class is obsolete")]
+public class Obsolete_Example
+{
+    [Obsolete("this method is old")]
+    private static void Method1()
+    {
+        Console.WriteLine("I'm method 1");
+    }
+    
+    private static void Method2()
+    {
+        Console.WriteLine("I'm method 2");
+    }
+
+    public static void Show()
+    {
+        Method1(); //generate a compiler warning
+        Method2();
+    }
+}
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class CustomAttribute_Example : Attribute
+{
+    public string Description { get; }
+
+
+    public CustomAttribute_Example(string description)
+    {
+        Description = description;
+    }
+}
+public class MyCustomAttribute
+{
+    [CustomAttribute_Example("this is my attribute")]
+    public static void CustomAttributeFunc()
+    {
+        Console.WriteLine("hello");
+    }
+
+}
+
+
+namespace _11_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conditional_Example.Show();
+
+            //Obsolete_Example.Show();
+
+            //MyCustomAttribute.CustomAttributeFunc();
+
+            Console.Read();
+        }
+    }
+}
---
+++ 12-Reflection/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _12_Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(int);
+
+            Console.WriteLine("Type Information:");
+            Console.WriteLine($"Name      : {type.Name}");
+            Console.WriteLine($"Full Name : {type.FullName}");
+            Console.WriteLine($"Base Type : {type.BaseType}");
+            Console.WriteLine($"Is Class  : {type.IsClass}");
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+/*
+ * Reflection in C# refers to the ability of a program to inspect its own structure, metadata, and behavior at runtime.
+
+Reflection in C# refers to the ability of a program to inspect, query, and interact with the metadata of types and members in an assembly at runtime. With reflection, you can obtain information about types, fields, properties, methods, and other members of your code dynamically, without knowing them at compile time.
+
+With reflection, you can dynamically load assemblies, examine and create types, and invoke methods or access properties, all without knowing them at compile-time. It provides a way to manipulate types, objects, and members dynamically.
+
+
+
+Key aspects of reflection in C# include:
+
+Type Information: You can retrieve information about a type, such as its name, namespace, methods, properties, fields, events, and more.
+Instantiation: You can create an instance of a type dynamically at runtime using the Activator.CreateInstance method.
+Method Invocation: You can invoke methods on an object dynamically, even if you don't know the method at compile time, using the Invoke method.
+Property and Field Access: You can get and set the values of properties and fields dynamically.
+Reflection is powerful but comes with some trade-offs:
+
+Performance: Reflection can have a performance overhead compared to statically typed code because it bypasses some of the optimizations performed by the compiler.
+Type Safety: Since reflection allows dynamic interaction with types, there's a risk of runtime errors if the code is not carefully designed.
+
+
+Practical uses of reflection:
+
+One practical use of reflection in C# is in the development of frameworks and tools where the structure of types is not known at compile time. Reflection enables these frameworks to dynamically discover and interact with types provided by user code. One such example is in the development of dependency injection containers.
+Another practical use of reflection in C# is in the development of serialization and deserialization frameworks. These frameworks convert objects into a format that can be easily stored, transmitted, or reconstructed, and reflection is often used to dynamically inspect and manipulate the structure of objects.
+Another practical use of reflection in C# is in the development of unit testing frameworks. Unit testing frameworks often utilize reflection to dynamically discover and execute test methods within test classes.
+Another practical use of reflection in C# is in the development of data access frameworks or Object-Relational Mapping (ORM) tools. Reflection can be employed to dynamically map object properties to database fields, allowing for flexible and generic data access without explicit knowledge of the underlying database schema.
+Another practical use of reflection in C# is in the development of user interface frameworks or libraries that need to dynamically generate or bind UI elements based on the properties of data objects.
+Another practical use of reflection in C# is in the implementation of extensibility mechanisms, where the application can dynamically discover and load extensions or plugins at runtime without having to know about them at compile time.
+Another practical use of reflection in C# is in the development of attribute-based validation frameworks. By utilizing custom attributes and reflection, you can create a system where validation rules are associated with class properties, making it easier to define and enforce data validation rules.
+Another practical use of reflection in C# is in the development of code generation tools or frameworks. Reflection allows you to inspect and analyze the structure of types at runtime, and this capability is often leveraged in code generation scenarios.
+Another practical use of reflection in C# is in certain design patterns.
+Reflection can also be utilized in the context of documentation generation, where you dynamically extract information about types, methods, properties, and other elements in your codebase to generate documentation automatically.
+and may other uses :-)
+ */
