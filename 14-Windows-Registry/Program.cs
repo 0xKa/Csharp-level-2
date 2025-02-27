051de7f4f4d7d2554968d599fb7148db4f91b028
Reda Hilal <reda0x04@gmail.com>
2025-02-27 11:39:20 +0400

    day 5

--- 14-Windows-Registry/Program.cs
+++ 14-Windows-Registry/Program.cs
@@ -44,6 +44,31 @@ namespace _14_Windows_Registry
 
         }
 
+        public static void DeleteFromRegistry(string keyPath, string valueName)
+        {
+            try
+            {
+                // Ensure we're using a relative path under HKCU
+                string relativeKeyPath = keyPath.Replace(@"HKEY_CURRENT_USER\", "");
+
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(relativeKeyPath, writable: true))
+                {
+                    if (key != null)
+                    {
+                        key.DeleteValue(valueName, throwOnMissingValue: false);
+                        Console.WriteLine($"Value '{valueName}' deleted successfully from '{keyPath}'.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Registry key '{keyPath}' not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
     }
 
 
@@ -52,14 +77,29 @@ namespace _14_Windows_Registry
         static void Main(string[] args)
         {
 
+
             // Specify the Registry key and path
             string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\zSampleKey";
-            string valueName = "Sample Name 2 :]";
-            string valueData = "Sample Data :)";
-            
-            WinRegistry.WritetoRegistry(keyPath, valueName, valueData);
+            string valueName = "sample___name";
+            string valueData = "sample___date";
 
+
+            WinRegistry.WritetoRegistry(keyPath, valueName,valueData);
             Console.WriteLine( WinRegistry.ReadFromRegistry(keyPath, valueName) );
+            WinRegistry.DeleteFromRegistry(keyPath, valueName);
+
+            // on Local Machine //
+            //string keyPath_local = @"HKEY_LOCAL_MACHINE\SOFTWARE\zSampleKeyLocal"; // on local machine, windows will save it in [WOW6432Node] folder
+            //string valueName_local = "MyName12341234";
+            //string valueData_local = "Sample Data for local machine:)";
+            
+            //WinRegistry.WritetoRegistry(keyPath_local, valueName_local, valueData_local);
+            //Console.WriteLine( WinRegistry.ReadFromRegistry(keyPath_local, valueName_local) );
+
+
+            // note: to write on local machine, the application needs admin permissions
+            // you can achiave that by running visual studio as admin,
+            // or by adding app.manifest file to the project to ask for permissions
 
             Console.ReadLine();
         }
